ff2903adbabad5efd3429228fe2e36ad6d1f078c
gleicekelly13 <gleice.kelly22@hotmail.com>
2022-09-01 00:17:22 -0300

    Alterações no html

---
+++ script.js
@@ -0,0 +1,40 @@
+    
+    
+    
+    /*
+    form.addEventListener('submit', (evento)=>{
+        evento.defaultPrevented()
+        checarNome()
+
+    })
+    function checarNome(){
+        const nome = fname.value 
+        console.log(nome)
+    }
+    
+    */
+    
+    
+    
+    
+    
+    
+    /*
+    
+    function nameValidate(fname){ 
+        if
+        (fname.value.length < 3) 
+        {
+            console.log('NOME DEVE TER 3 CARACTERES');
+        }
+        
+
+        if(campos[0].value.length < 3)
+        {
+            console.log('NOME DEVE TER 3 CARACTERES');
+        }
+        else
+        {
+            console.log('VALIDADO O NOME');
+        } */
+    
